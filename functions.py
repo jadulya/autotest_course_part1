5ff0fd9eb0099bf5fdd8cbb28efa8e0b41964624
jadulya <nelly6795@mail.ru>
2021-09-06 23:15:01 +0300

    HW3 (положила гитигнор, где он должен лежать)

--- functions.py
+++ functions.py
@@ -1,12 +1,14 @@
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
+
 def login(browser):
     email = browser.find_element(By.NAME, "email")
     email.send_keys("qa_test@test.ru")
     password = browser.find_element(By.NAME, "password")
     password.send_keys("!QAZ2wsx")
     browser.find_element(By.CLASS_NAME, "button").click()
+
 
 def element_is_present(browser, by, value):
     try:
