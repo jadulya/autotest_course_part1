e8efc5cb66852497bc214f83788c873e1b4a69eb
jadulya <nelly6795@mail.ru>
2021-09-10 20:16:47 +0300

    HW4 (1 тест из двух)

---
+++ test_hw4.py
@@ -0,0 +1,34 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+
+from functions import login, element_is_present
+
+
+def test_about_page():
+    with Chrome() as browser:
+        browser.get("https://qastand.valhalla.pw/about")
+        browser.maximize_window()
+        login(browser)
+        browser.find_element(By.NAME, "name").send_keys("Неля")
+        browser.find_element(By.NAME, "surname").send_keys("Аюпова")
+        browser.find_element(By.ID, "age2").click()
+        browser.find_element(By.ID, "lang1").click()
+        browser.find_element(By.ID, "lang2").click()
+        level = browser.find_element(By.NAME, "lvl")
+        select = Select(level)
+        select.select_by_visible_text("Middle")
+        browser.find_element(By.NAME, "surname").send_keys(Keys.ENTER)
+        success_text = browser.find_element(By.CSS_SELECTOR, ".notification.is-success").text
+
+        assert browser.find_element(By.ID, "age1").get_attribute("checked"), "Чек бокс не включен по умолчанию"
+        assert element_is_present(browser, By.CSS_SELECTOR, ".notification.is-success"), \
+            "Уведомление об успехе отсутствует"
+        assert success_text == "Успех."
+
+def test_upload_file_page():
+    with Chrome() as browser:
+        browser.get("https://qastand.valhalla.pw/upload_file")
+        browser.maximize_window()
+        login(browser)
