a21d51092e448afa92d448115e47eb15a5ad7d43
jadulya <nelly6795@mail.ru>
2021-09-16 11:46:13 +0300

    рефакторинг с ожиданиями функций и дз3

--- functions.py
+++ functions.py
@@ -1,18 +1,30 @@
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
-
-def login(browser):
-    email = browser.find_element(By.NAME, "email")
-    email.send_keys("qa_test@test.ru")
-    password = browser.find_element(By.NAME, "password")
-    password.send_keys("!QAZ2wsx")
-    browser.find_element(By.CLASS_NAME, "button").click()
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 def element_is_present(browser, by, value):
     try:
-        browser.find_element(by, value)
+        wait_until_visible(browser, (by, value))
         return True
-    except NoSuchElementException:
+    except TimeoutException:
         return False
+
+
+def wait_until_clickable(browser, locator, timeout=10):
+    return WebDriverWait(browser, timeout).until(ec.element_to_be_clickable(locator))
+
+
+def wait_until_visible(browser, locator, timeout=10):
+    return WebDriverWait(browser, timeout).until(ec.visibility_of_element_located(locator))
+
+
+def wait_until_all_elements_visible(browser, locator, timeout=10):
+    return WebDriverWait(browser, timeout).until(ec.visibility_of_all_elements_located(locator))
+
+
+def login(browser):
+    wait_until_clickable(browser, (By.NAME, "email")).send_keys("qa_test@test.ru")
+    wait_until_clickable(browser, (By.NAME, "password")).send_keys("!QAZ2wsx")
+    wait_until_clickable(browser, (By.CLASS_NAME, "button")).click()
